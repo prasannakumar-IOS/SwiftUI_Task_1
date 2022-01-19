70f5c45350e2574c5221220721df7aa55fe1af8d
Prasannakumar Manikandan <prasannakumar@mallow-tech.com>
2022-01-19 10:42:49 +0530

    Changes in alignments

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -16,104 +16,104 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             VStack {
-            ZStack(alignment: .top) {
-                Color("LSBackGround")
+                ZStack(alignment: .top) {
+                    Color("LSBackGround")
                         .ignoresSafeArea(.all)
-                Group {
-                    Circle()
-                        .fill(Color("LSDandelion"))
-                        .frame(width: 350, height: 400)
-                        .offset(x: -230, y: -250)
-                    Circle()
-                        .fill(Color("LSLightOrange"))
-                        .frame(width: 150, height: 150)
-                        .offset(x: -180, y: 80)
-                    Circle()
-                        .fill(Color("LSDarkBlue"))
-                        .frame(width: 80, height: 100)
-                        .offset(x: -90, y: 100)
-                    Capsule()
-                        .fill(Color("LSCapsuleColor"))
-                        .frame(width: 140, height: 200)
-                        .offset(x: -45, y: -200)
-                    Circle()
-                        .fill(Color("LSTruquBlue"))
-                        .frame(width: 100, height: 110)
-                        .offset(x: -45, y: -30)
-                    Circle()
-                        .fill(Color("LSLightOrange"))
-                        .frame(width: 130, height: 150)
-                        .offset(x: -45, y: -120)
-                    Circle()
-                        .fill(Color("LSLightPink"))
-                        .frame(width: 190, height: 200)
-                        .offset(x: 130, y: -200)
-                    customView()
-                        .offset(x: 40, y: 60)
-                    customRightView()
-                        .offset(x: 180, y: 60)
-                    Circle()
-                        .fill(Color("LSDarkPink"))
-                        .frame(width: 165, height: 250)
-                        .offset(x: 120, y: -80)
-                    }
-                VStack(spacing: 10) {
-                    Spacer()
                     Group {
-                        VStack(spacing: 0) {
-                            Text("Welcome")
+                        Circle()
+                            .fill(Color("LSDandelion"))
+                            .frame(width: 350, height: 400)
+                            .offset(x: -230, y: -250)
+                        Circle()
+                            .fill(Color("LSLightOrange"))
+                            .frame(width: 150, height: 150)
+                            .offset(x: -180, y: 80)
+                        Circle()
+                            .fill(Color("LSDarkBlue"))
+                            .frame(width: 80, height: 100)
+                            .offset(x: -90, y: 100)
+                        Capsule()
+                            .fill(Color("LSCapsuleColor"))
+                            .frame(width: 140, height: 200)
+                            .offset(x: -45, y: -200)
+                        Circle()
+                            .fill(Color("LSTruquBlue"))
+                            .frame(width: 100, height: 110)
+                            .offset(x: -45, y: -30)
+                        Circle()
+                            .fill(Color("LSLightOrange"))
+                            .frame(width: 130, height: 150)
+                            .offset(x: -45, y: -120)
+                        Circle()
+                            .fill(Color("LSLightPink"))
+                            .frame(width: 190, height: 200)
+                            .offset(x: 130, y: -200)
+                        customView()
+                            .offset(x: 40, y: 60)
+                        customRightView()
+                            .offset(x: 180, y: 60)
+                        Circle()
+                            .fill(Color("LSDarkPink"))
+                            .frame(width: 165, height: 250)
+                            .offset(x: 120, y: -80)
+                    }
+                    VStack(spacing: 10) {
+                        Spacer()
+                        Group {
+                            VStack(spacing: 0) {
+                                Text("Welcome")
+                                    .foregroundColor(.white)
+                                    .font(.custom("Pacifico-Regular", size: 40))
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                    .frame(height: 30)
+                                Text("Back")
+                                    .foregroundColor(.white)
+                                    .font(.custom("Pacifico-Regular", size: 40))
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                            }
+                            TextField("Name", text: $name)
+                                .padding(EdgeInsets(top: 0, leading: 0, bottom: 10, trailing: 0))
+                            SecureField("PassWord", text: $password)
                                 .foregroundColor(.white)
-                                .font(.custom("Pacifico-Regular", size: 40))
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                                .frame(height: 30)
-                            Text("Back")
-                                .foregroundColor(.white)
-                                .font(.custom("Pacifico-Regular", size: 40))
-                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .padding(EdgeInsets(top: 0, leading: 0, bottom: 10, trailing: 0))
+                            Button(action: {
+                                
+                            }) {
+                                Text("Forget password?")
+                                    .font(.custom("Oswald-Regular", size: 18))
+                                    .foregroundColor(.white)
+                                    .frame(maxWidth: .infinity, alignment: .trailing)
+                                    .padding(EdgeInsets(top: 0, leading: 0, bottom: 10, trailing: 0))
+                            }
+                            Button(action: {
+                                
+                            }) {
+                                Text("Login")
+                                    .font(Font.system(size: 20, weight: .medium, design: .default))
+                                    .frame(width: 120, height: 50)
+                                    .foregroundColor(.white)
+                                    .background(Color("LSButtonColour"))
+                                
+                            }.cornerRadius(20)
                         }
-                        TextField("Name", text: $name)
-                            .padding(EdgeInsets(top: 0, leading: 0, bottom: 10, trailing: 0))
-                        SecureField("PassWord", text: $password)
-                            .foregroundColor(.white)
-                            .padding(EdgeInsets(top: 0, leading: 0, bottom: 10, trailing: 0))
-                        Button(action: {
-
-                        }) {
-                            Text("Forget password?")
+                        .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20))
+                        HStack {
+                            Text("New User?")
                                 .font(.custom("Oswald-Regular", size: 18))
                                 .foregroundColor(.white)
-                                .frame(maxWidth: .infinity, alignment: .trailing)
-                                .padding(EdgeInsets(top: 0, leading: 0, bottom: 10, trailing: 0))
-                        }
-                        Button(action: {
-
-                        }) {
-                            Text("Login")
-                                .font(Font.system(size: 20, weight: .medium, design: .default))
-                                .frame(width: 120, height: 50)
-                                .foregroundColor(.white)
-                                .background(Color("LSButtonColour"))
-                            
-                        }.cornerRadius(20)
-                    }
-                    .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20))
-                    HStack {
-                        Text("New User?")
-                            .font(.custom("Oswald-Regular", size: 18))
-                            .foregroundColor(.white)
-                        NavigationLink(destination: SignUpView(), isActive: $isNaviteOk) {
-                            Button(action: {
-                                self.isNaviteOk = true
-                            }) {
-                                Text("Signup")
-                                    .font(.custom("Oswald-Regular", size: 20))
-                                    .foregroundColor(Color("LSButtonColour"))
-
+                            NavigationLink(destination: SignUpView(), isActive: $isNaviteOk) {
+                                Button(action: {
+                                    self.isNaviteOk = true
+                                }) {
+                                    Text("Signup")
+                                        .font(.custom("Oswald-Regular", size: 20))
+                                        .foregroundColor(Color("LSButtonColour"))
+                                    
+                                }
                             }
-                        }
-                    }.padding(EdgeInsets(top: 15, leading: 0, bottom: 0, trailing: 0))
-                }.textFieldStyle(LStextFieldStyle())
-            }
+                        }.padding(EdgeInsets(top: 15, leading: 0, bottom: 0, trailing: 0))
+                    }.textFieldStyle(LStextFieldStyle())
+                }
             }
         }
         .navigationBarHidden(true)
--- Shared/SignUpView.swift
+++ Shared/SignUpView.swift
@@ -16,67 +16,67 @@ struct SignUpView: View {
     @Environment(\.dismiss) private var dismiss
     
     var body: some View {
-            ZStack(alignment: .top) {
-                Color("LSBackGround")
-                        .ignoresSafeArea(.all)
-                VStack {
-                    Group {
-                        Spacer()
-                            .frame(height: 35)
-                        Text("Hello...!")
-                            .foregroundColor(.white)
-                            .font(.custom("Pacifico-Regular", size: 30))
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                        TextField("FirstName", text: $firstName)
-                        TextField("LastName", text: $lastName)
-                        TextField("email", text: $email)
-                    }
-                    .textFieldStyle(LStextFieldStyle())
-                    .padding(EdgeInsets(top: 0, leading: 20, bottom: 15, trailing: 20))
-                    LSCustomTextFieldForNumber(placeHolder: "Phone Number")
-                        .padding(EdgeInsets(top: 0, leading: 20, bottom: 15, trailing: 20))
-                    Group {
-                        TextField("Select Centre", text: $selectCentre)
-                    }
-                    .textFieldStyle(LStextFieldStyle())
-                    .padding(EdgeInsets(top: 0, leading: 20, bottom: 15, trailing: 20))
-                    Button(action: {
-
-                    }) {
-                        Text("SignUp")
-                            .font(Font.system(size: 20, weight: .medium, design: .default))
-                            .frame(width: 120, height: 50)
-                            .foregroundColor(.white)
-                            .background(Color("LSButtonColour"))
-                        
-                    }.cornerRadius(20)
-                    .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20))
+        ZStack(alignment: .top) {
+            Color("LSBackGround")
+                .ignoresSafeArea(.all)
+            VStack {
+                Group {
                     Spacer()
-                    HStack {
-                        Text("Already Have Accout?")
-                            .font(.custom("Oswald-Regular", size: 15))
-                            .foregroundColor(.white)
-                        Spacer()
-                            .frame(width: 2)
-                        Button(action: {
-                            dismiss()
-                        }) {
-                            Text("Login")
-                                .font(.custom("Oswald-Regular", size: 20))
-                                .foregroundColor(Color("LSButtonColour"))
-                            }
-                        }
+                        .frame(height: 35)
+                    Text("Hello...!")
+                        .foregroundColor(.white)
+                        .font(.custom("Pacifico-Regular", size: 30))
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    TextField("FirstName", text: $firstName)
+                    TextField("LastName", text: $lastName)
+                    TextField("email", text: $email)
                 }
-                Circle()
-                    .fill(Color("LSDandelion"))
-                    .frame(width: 150, height: 200)
-                    .offset(x: 200, y: 710)
-                Circle()
-                    .fill(Color("LSLightOrange"))
-                    .frame(width: 150, height: 150)
-                    .offset(x: -200, y: 700)
+                .textFieldStyle(LStextFieldStyle())
+                .padding(EdgeInsets(top: 0, leading: 20, bottom: 15, trailing: 20))
+                LSCustomTextFieldForNumber(placeHolder: "Phone Number")
+                    .padding(EdgeInsets(top: 0, leading: 20, bottom: 15, trailing: 20))
+                Group {
+                    TextField("Select Centre", text: $selectCentre)
+                }
+                .textFieldStyle(LStextFieldStyle())
+                .padding(EdgeInsets(top: 0, leading: 20, bottom: 15, trailing: 20))
+                Button(action: {
+                    
+                }) {
+                    Text("SignUp")
+                        .font(Font.system(size: 20, weight: .medium, design: .default))
+                        .frame(width: 120, height: 50)
+                        .foregroundColor(.white)
+                        .background(Color("LSButtonColour"))
+                    
+                }.cornerRadius(20)
+                    .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20))
+                Spacer()
+                HStack {
+                    Text("Already Have Accout?")
+                        .font(.custom("Oswald-Regular", size: 15))
+                        .foregroundColor(.white)
+                    Spacer()
+                        .frame(width: 2)
+                    Button(action: {
+                        dismiss()
+                    }) {
+                        Text("Login")
+                            .font(.custom("Oswald-Regular", size: 20))
+                            .foregroundColor(Color("LSButtonColour"))
+                    }
+                }
             }
-            .navigationBarHidden(true)
+            Circle()
+                .fill(Color("LSDandelion"))
+                .frame(width: 150, height: 200)
+                .offset(x: 200, y: 710)
+            Circle()
+                .fill(Color("LSLightOrange"))
+                .frame(width: 150, height: 150)
+                .offset(x: -200, y: 700)
+        }
+        .navigationBarHidden(true)
     }
 }
 
